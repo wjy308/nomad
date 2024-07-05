6922d014abe31915cb8a1d4e5ae539959251f29b
unknown <wjy308@nate.com>
2024-07-05 14:31:00 +0900

    PR Test 변경사항 롤백

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,6 @@ export default function Home() {
     // TODO: ~~한 이유로 console.log를 남김!
     // eslint-disable-next-line no-console
     console.log('state', state);
-    console.log('dddd);
   }, [state]);
 
   return (
