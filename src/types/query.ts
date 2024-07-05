b3fe59ccae55fae9047577dfa58cb1a59e1393f2
unknown <wjy308@nate.com>
2024-07-05 15:44:18 +0900

    feat: 폴더구조 정리

--- src/types/query.ts
+++
@@ -1,4 +0,0 @@
-import { UseMutationOptions } from '@tanstack/react-query';
-import { AxiosError } from 'axios';
-
-export type MutationOptions<TReq = unknown, TRes = unknown> = Omit<UseMutationOptions<TRes, AxiosError, TReq>, 'mutationKey' | 'mutationFn'>;
