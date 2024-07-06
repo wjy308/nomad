a9bdd5e098dec12e2c0b33bd46ab034881faa14a
waterkail <41028065+waterkail@users.noreply.github.com>
2024-07-06 16:28:35 +0900

    feat: 내 체험 관리 카드내용 UI 완성

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -9,11 +9,11 @@ interface Props {
 /** 카드 컴포넌트 imgae : 카드 이미지, children: 카드 내용 컴포넌트 넣으면 됨 */
 export default function Card({ image, children }: Props) {
   return (
-    <div className='rounded-[1.2rem] max-w-[79.2rem] w-full flex gap-x-[2.4rem] h-[20.4rem] max-lg:h-[15.6rem] max-md:h-[12.8rem] max-lg:gap-x-[1.2rem] max-md:gap-x-[0.8rem]'>
-      <div className='w-[20.4rem] h-[20.4rem] max-lg:w-[15.6rem] max-lg:h-[15.6rem] max-md:w-[12.8rem] max-md:h-[12.8rem]'>
+    <div className='overflow-hidden rounded-[1.2rem] max-w-[79.2rem] w-full flex gap-x-[2.4rem] h-[20.4rem] max-lg:h-[15.6rem] max-md:h-[12.8rem] max-lg:gap-x-[1.2rem] max-md:gap-x-[0.8rem]'>
+      <div className='relative w-[20.4rem] h-[20.4rem] max-lg:w-[15.6rem] max-lg:h-[15.6rem] max-md:w-[12.8rem] max-md:h-[12.8rem]'>
         <Image src={image} alt='' fill />
       </div>
-      <div className='pr-[2.4rem] flex justify-center items-center max-lg:pr-[1.8rem] max-md:pr-[1.4rem]'>{children}</div>
+      <div className='pr-[2.4rem] flex flex-grow max-lg:pr-[1.8rem] max-md:pr-[1.4rem]'>{children}</div>
     </div>
   );
 }
--- src/components/Card/myActibityCardInfo/index.tsx
+++ src/components/Card/myActibityCardInfo/index.tsx
@@ -1,18 +1,28 @@
-// interface MyActibityCardData {
-//   id: number;
-//   userId: number;
-//   title: string;
-//   description: string;
-//   category: string;
-//   price: number;
-//   address: string;
-//   bannerImageUrl: string;
-//   rating: number;
-//   reviewCount: number;
-//   createdAt: string;
-//   updatedAt: string;
-// }
+import Image from 'next/image';
 
-export default function MyActibitiyCardInfo() {
-  return <div>티그사골탕</div>;
+interface MyActibityCardData {
+  title: string;
+  price: number;
+  rating: number;
+  reviewCount: number;
+}
+
+export default function MyActibitiyCardInfo({ title, price, rating, reviewCount }: MyActibityCardData) {
+  return (
+    <div className='flex flex-col self-center justify-center w-full h-[16.2rem] max-lg:h-[13.1rem] max-sm:h-[10.4rem]'>
+      <div className='flex gap-x-[0.6rem]'>
+        <div className='relative w-[1.9rem] h-[1.9rem] max-md:w-[1.6rem] max-md:h-[1.6rem]'>
+          <Image src='/svgs/star.svg' fill alt='' />
+        </div>
+        <span className='text-[#1b1b1b] leading-[1.9rem] text-[1.6rem] max-md:text-[1.4rem] max-md:leading-[1.6rem]'>{`${rating} (${reviewCount})`}</span>
+      </div>
+      <h3 className='mt-[0.6rem] text-[2rem] font-bold leading-[2.6rem] text-[#121] max-lg:text-[1.8rem] max-md:text-[1.4rem] max-md:mt-0'>{title}</h3>
+      <div className='flex justify-between mt-[7.2rem] max-lg:mt-[4.8rem] max-md:mt-[2.9rem]'>
+        <span className='text-[2.4rem] text-[#1b1b1b] leading-[4rem] font-medium max-lg:text-[2rem] max-md:text-[1.6rem] max-md:leading-[3.2rem]'>₩{price.toLocaleString('ko-KR')}</span>
+        <button type='button' className='relative w-[4rem] h-[4rem] max-md:w-[3.2rem] max-md:h-[3.2rem]'>
+          <Image src='/svgs/icon_meatball.svg' fill alt='' />
+        </button>
+      </div>
+    </div>
+  );
 }
