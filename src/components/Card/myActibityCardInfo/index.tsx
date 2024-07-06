f086efbd5f32fbee9e93dd7b9e066c01e309e57e
waterkail <41028065+waterkail@users.noreply.github.com>
2024-07-06 18:39:44 +0900

    feat: 내 체험 관리 카드내용 UI 완성

--- src/components/Card/myActibityCardInfo/index.tsx
+++ src/components/Card/myActibityCardInfo/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 interface MyActibityCardData {
   title: string;
@@ -8,8 +9,18 @@ interface MyActibityCardData {
 }
 
 export default function MyActibitiyCardInfo({ title, price, rating, reviewCount }: MyActibityCardData) {
+  const [isDropShow, setIsDropShow] = useState<boolean>(false);
+
+  const closeDrop = () => {
+    setIsDropShow(false);
+  };
+
+  useEffect(() => {
+    document.body.addEventListener('click', closeDrop);
+  }, []);
+
   return (
-    <div className='flex flex-col self-center justify-center w-full h-[16.2rem] max-lg:h-[13.1rem] max-sm:h-[10.4rem]'>
+    <div className='relative flex flex-col self-center justify-center w-full h-[16.2rem] max-lg:h-[13.1rem] max-sm:h-[10.4rem]'>
       <div className='flex gap-x-[0.6rem]'>
         <div className='relative w-[1.9rem] h-[1.9rem] max-md:w-[1.6rem] max-md:h-[1.6rem]'>
           <Image src='/svgs/star.svg' fill alt='' />
@@ -19,10 +30,33 @@ export default function MyActibitiyCardInfo({ title, price, rating, reviewCount 
       <h3 className='mt-[0.6rem] text-[2rem] font-bold leading-[2.6rem] text-[#121] max-lg:text-[1.8rem] max-md:text-[1.4rem] max-md:mt-0'>{title}</h3>
       <div className='flex justify-between mt-[7.2rem] max-lg:mt-[4.8rem] max-md:mt-[2.9rem]'>
         <span className='text-[2.4rem] text-[#1b1b1b] leading-[4rem] font-medium max-lg:text-[2rem] max-md:text-[1.6rem] max-md:leading-[3.2rem]'>₩{price.toLocaleString('ko-KR')}</span>
-        <button type='button' className='relative w-[4rem] h-[4rem] max-md:w-[3.2rem] max-md:h-[3.2rem]'>
+        <button
+          type='button'
+          onClick={(e) => {
+            e.stopPropagation();
+            setIsDropShow(!isDropShow);
+          }}
+          className='relative w-[4rem] h-[4rem] max-md:w-[3.2rem] max-md:h-[3.2rem]'
+        >
           <Image src='/svgs/icon_meatball.svg' fill alt='' />
         </button>
       </div>
+      {isDropShow && (
+        <div className='absolute bottom-[-12.7rem] right-0 flex flex-col rounded-md border-[#DDD] border max-lg:bottom-[-11.7rem] max-md:bottom-[-9.3rem]'>
+          <button
+            type='button'
+            className='flex justify-center items-center px-[4.6rem] py-[1.8rem] text-[1.8rem] text-[#4B4B4B] leading-[2.2rem] font-medium border-[#DDD] border-b max-lg:text-[1.6rem] max-lg:px-[3.4rem] max-lg:py-[1.4rem] max-md:text-[1.2rem] max-md:px-[2.8rem] max-md:py-[1rem]'
+          >
+            수정하기
+          </button>
+          <button
+            type='button'
+            className='flex justify-center items-center text-[1.8rem] text-[#4B4B4B] font-medium px-[4.6rem] leading-[2.2rem] py-[1.8rem] max-lg:text-[1.6rem] max-lg:px-[3.4rem] max-lg:py-[1.4rem] max-md:text-[1.2rem] max-md:px-[2.8rem] max-md:py-[1rem]'
+          >
+            삭제하기
+          </button>
+        </div>
+      )}
     </div>
   );
 }
