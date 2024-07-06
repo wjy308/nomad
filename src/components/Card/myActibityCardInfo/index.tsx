b01886a9cd8226e1af51a08d331b0e9acf9bbbd3
waterkail <41028065+waterkail@users.noreply.github.com>
2024-07-06 11:19:56 +0900

    fix: 반응형 사이즈 변경

--- src/components/Card/myActibityCardInfo/index.tsx
+++ src/components/Card/myActibityCardInfo/index.tsx
@@ -1,3 +1,18 @@
+// interface MyActibityCardData {
+//   id: number;
+//   userId: number;
+//   title: string;
+//   description: string;
+//   category: string;
+//   price: number;
+//   address: string;
+//   bannerImageUrl: string;
+//   rating: number;
+//   reviewCount: number;
+//   createdAt: string;
+//   updatedAt: string;
+// }
+
 export default function MyActibitiyCardInfo() {
   return <div>티그사골탕</div>;
 }
