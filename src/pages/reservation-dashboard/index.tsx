fc812162fa4f33c858e93ec644415b26ca64adfc
unknown <wjy308@nate.com>
2024-07-09 21:53:31 +0900

    feat: 예약현황 pc사이즈일때만 footer 보이게끔 추가

--- src/pages/reservation-dashboard/index.tsx
+++ src/pages/reservation-dashboard/index.tsx
@@ -1,11 +1,24 @@
+import { useEffect, useState } from 'react';
+import Footer from '@/components/Layout/Footer';
+import visibleFooter from './visibleFooter';
+
 export const getStaticProps = async () => ({
   props: {
     layoutType: 'removeFooter',
   },
 });
 
-function Index() {
-  return <main>예약현황</main>;
+export function Index() {
+  const [isMobileOrTablet, setIsMobileOrTablet] = useState(true);
+
+  useEffect(() => visibleFooter(setIsMobileOrTablet), []);
+
+  return (
+    <>
+      <main>예약현황</main>
+      {isMobileOrTablet ? null : <Footer />} {/* 모바일 및 태블릿 사이즈일 때는 Footer 숨김 */}
+    </>
+  );
 }
 
 export default Index;
