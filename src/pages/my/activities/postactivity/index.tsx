b24c565d5b2daea0d00375066f7ca6e7c9b924bd
waterkail <41028065+waterkail@users.noreply.github.com>
2024-07-18 14:48:54 +0900

    style: 일부 input 스타일 수정 및 내 체험 등록/수정 스타일 적용

--- src/pages/my/activities/postactivity/index.tsx
+++ src/pages/my/activities/postactivity/index.tsx
@@ -59,6 +59,7 @@ export default function PostActivitiy() {
   const DATE_LABEL_STYLE = 'text-[2rem] leading-[2.6rem] text-[#4b4b4b] max-md:text-[1.6rem]';
   const DATE_INPUT_LABEL_STYLE = 'flex flex-col gap-y-[1rem] max-md:gap-y-[0.8rem]';
   const LABEL_STYLE = 'text-[#1b1b1b] text-[2.4rem] font-bold leading-[2.6rem] max-md:text-[2rem]';
+  const INPUT_STYLE = 'h-[5.6rem] leading-[2.6rem] py-[0.8rem] px-[1.6rem]';
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -153,7 +154,7 @@ export default function PostActivitiy() {
   return (
     <ModalContextProvider>
       <MyLayout>
-        <main className='bg-[#fafafa] mb-[27rem] max-lg:mb-[40rem] max-md:mb-[13.6rem]'>
+        <main className='bg-[#fafafa] mb-[27rem] max-lg:mb-[40rem] max-md:mb-[13.6rem] text-[1.6rem]'>
           <form onSubmit={handleSubmit}>
             <div className='flex justify-between mb-[2.4rem] '>
               <h2 className='text-[3.2rem] text-[#000] leading-[3.8rem] font-bold'>내 체험 등록</h2>
@@ -161,13 +162,14 @@ export default function PostActivitiy() {
             </div>
             <div className='flex flex-col gap-y-[2.4rem]'>
               {/* ------제목------ */}
-              <Input placeholder='제목' type='text' id='title' onBlur={(e) => onBlurSetData(e, 'title')} />
+              <Input placeholder='제목' type='text' id='title' onBlur={(e) => onBlurSetData(e, 'title')} cssName={INPUT_STYLE} />
               {/* ------카테고리------ */}
               <Dropdown
                 lists={categories}
                 name='카테고리'
                 placeholder='카테고리'
                 id='category'
+                inputHeight='5.6rem'
                 onSelectedId={(id) => {
                   const selected = categories.filter((category) => category.id === id);
                   setPostData((prev) => ({ ...prev, category: selected[0].category }));
@@ -180,21 +182,21 @@ export default function PostActivitiy() {
                 <label htmlFor='price' className={LABEL_STYLE}>
                   가격
                 </label>
-                <Input placeholder='가격' type='number' id='price' onBlur={(e) => onBlurSetData(e, 'price')} />
+                <Input placeholder='가격' type='text' id='price' onBlur={(e) => onBlurSetData(e, 'price')} cssName={INPUT_STYLE} />
               </div>
               {/* -----주소----- */}
               <div className='flex flex-col gap-y-[1.6rem]'>
                 <label htmlFor='address' className={LABEL_STYLE}>
                   주소
                 </label>
-                <Input type='text' placeholder='주소를 입력해주세요' id='address' value={postData.address} ref={addressRef} readOnly onClick={openAddress} />
+                <Input type='text' placeholder='주소를 입력해주세요' id='address' value={postData.address} ref={addressRef} readOnly onClick={openAddress} cssName={INPUT_STYLE} />
               </div>
               {/* ------예약 가능한 시간대------ */}
               <div className='flex flex-col gap-y-[2.4rem]'>
                 <span className={`${LABEL_STYLE} max-md:hidden`}>예약 가능한 시간대</span>
                 <div className='flex flex-col gap-y-[2.1rem] max-lg:gap-y-[1.6rem]'>
                   <div className='flex pb-[2.1rem] border-b border-[#DDD] max-lg:pb-[1.6rem]'>
-                    <div className={`${DATE_INPUT_LABEL_STYLE} mr-[2rem]`}>
+                    <div className={`${DATE_INPUT_LABEL_STYLE} mr-[2rem] flex-grow`}>
                       <label htmlFor='date' className={DATE_LABEL_STYLE}>
                         날짜
                       </label>
@@ -202,6 +204,7 @@ export default function PostActivitiy() {
                         name='날짜'
                         id='date'
                         value={schedule.date}
+                        cssName='h-[5.6rem] px-[1.6rem] max-md:h-[4.4rem]'
                         onPostDataValue={(date) => {
                           setSchedule((prev) => ({ ...prev, date }));
                         }}
@@ -215,6 +218,7 @@ export default function PostActivitiy() {
                         type='time'
                         id='startTime'
                         value={schedule.startTime}
+                        cssName='h-[5.6rem] px-[1.6rem] max-md:h-[4.4rem]'
                         onChange={(e) => {
                           setSchedule((prev) => ({ ...prev, startTime: e.target.value }));
                         }}
@@ -229,6 +233,7 @@ export default function PostActivitiy() {
                         type='time'
                         id='endTime'
                         value={schedule.endTime}
+                        cssName='h-[5.6rem] px-[1.6rem] max-md:h-[4.4rem]'
                         onChange={(e) => {
                           setSchedule((prev) => ({ ...prev, endTime: e.target.value }));
                         }}
