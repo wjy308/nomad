4933e002b89e060dc8d81f7a9f8721ab1932e78a
unknown <wjy308@nate.com>
2024-07-18 20:22:53 +0900

    feat:예약현황 드롭박스 제작

--- src/pages/reservationDashboard/ActivityDropDown.tsx
+++ src/pages/reservationDashboard/ActivityDropDown.tsx
@@ -1,20 +1,61 @@
-// /* eslint-disable jsx-a11y/label-has-associated-control */
-// function ActivityDropDown() {
-//   return (
-//     <div className='flex flex-col relative'>
-//       <label className='absolute top-[-9px] px-1 bg-white left-5 '>체험명</label>
-//       <input
-//         type='text'
-//         className='border border-black pl-4 py-5 pr-10 rounded outline-none dark:bg-darkMode-black-20 dark:text-white whitespace-normal overflow-hidden text-ellipsis'
-//         /* value={selectedActivityTitle || '' } */
-//       />
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/16/solid';
+import { useState } from 'react';
 
-//       {/* 버튼 클릭시 모달 열기 */}
-//       <button type='button' className='cursor-pointer' /* onClick={handleClickDropDown} */>
-//         <img src='/assets/chevron_down.svg' alt='dropdown_icon' className='w-6 absolute top-5 right-3' />
-//       </button>
-//     </div>
-//   );
-// }
+interface ActivityType {
+  id: number;
+  title: string;
+  category: string;
+  //   onItemSelected: (item: string) => void;
+}
+interface ActivityDropDownProps {
+  items: ActivityType[];
+}
 
-// export default ActivityDropDown;
+/* eslint-disable jsx-a11y/label-has-associated-control */
+function ActivityDropDown({ items }: ActivityDropDownProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState<string | null>(null);
+
+  const handleButtonClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleItemClick = (item: string) => {
+    setSelectedItem(item);
+    setIsOpen(false);
+  };
+
+  return (
+    <div className='flex flex-col relative'>
+      <label className='absolute top-[-0.65rem] pl-[0.5rem] pr-[0.5rem] left-[1rem] bg-[white] text-[black] z-10'>체험명</label>
+      <button
+        type='button'
+        className='relative border border-black h-[5.6rem] text-left text-[black] text-[1.6rem] bg-[white] pl-[1.6rem] py-[1rem] pr-[2.5rem] rounded outline-none overflow-hidden text-ellipsis'
+        onClick={handleButtonClick}
+      >
+        <div className='absolute flex flex-row w-[2.4rem] h-[2.4rem] text-[black] right-[1rem] bottom-[1.5rem]' /* onClick={handleClickDropDown} */>
+          {isOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
+        </div>
+        {selectedItem || '선택'}
+      </button>
+      {isOpen && (
+        <div className='absolute mt-[5.6rem] border-[black] border-[0.1rem] w-full rounded-md bg-white shadow-lg z-10 text-[black]'>
+          <ul className='pt-[0.25rem]'>
+            {items.map((item) => (
+              // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions, jsx-a11y/click-events-have-key-events
+              <li
+                key={item.id}
+                className='h-[5.6rem] flex items-center text-[1.6rem] cursor-pointer border-b select-none relative py-[0.5rem] pl-[1.75rem] pr-[2.25rem] hover:bg-gray-100'
+                onClick={() => handleItemClick(item.title)}
+              >
+                {item.title}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default ActivityDropDown;
--- src/pages/reservationDashboard/index.tsx
+++ src/pages/reservationDashboard/index.tsx
@@ -1,20 +1,31 @@
-import Dropdown from '@/components/Dropdown';
 import getMyActivities from '@/apis/get/getMyActivities';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Calendar2 from './calendar';
+import ActivityDropDown from './ActivityDropDown';
 
+interface ActivityType {
+  id: number;
+  title: string;
+  category: string;
+}
 function Index() {
-  const list: { id: number; category: string; title?: string }[] = [];
+  const [activityLists, setActivityLists] = useState<ActivityType[]>([]);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     async function getMyAct() {
       try {
-        const data = await getMyActivities({ size: 6 });
-        // eslint-disable-next-line no-console
-        console.log(data);
+        const data = await getMyActivities({ size: undefined });
+        const activities = data?.activities.map((activity: ActivityType) => ({
+          id: activity.id,
+          title: activity.title,
+          category: activity.category,
+        }));
+        if (activities) {
+          setActivityLists(activities);
+        }
       } catch (err) {
-        // eslint-disable-next-line no-console
-        console.error('err');
+        setError('데이터 로딩 중 에러가 발생했습니다.');
       }
     }
     getMyAct();
@@ -23,7 +34,8 @@ function Index() {
   return (
     <div className='flex py-[1.6rem] px-[1.6rem] flex-col'>
       <h1 className='text-[#000] text-[3.2rem] font-[700] mb-[4.2rem]'>예약 현황</h1>
-      <Dropdown lists={list} name='카테고리' placeholder='카테고리' onSelectedId={() => {}} />
+      {error && <p className='text-red-500'>{error}</p>}
+      <ActivityDropDown items={activityLists} />
       <div className='container mx-auto p-4'>
         <Calendar2 />
       </div>
