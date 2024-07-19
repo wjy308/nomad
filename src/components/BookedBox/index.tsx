0fc4e3e717f2fd3395e25cf4fe39b83aa4e50925
unknown <wjy308@nate.com>
2024-07-19 22:02:44 +0900

    feat:예약현황 상태박스 api연동 및 수정

--- src/components/BookedBox/index.tsx
+++ src/components/BookedBox/index.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export type Reservation = {
   date: string;
   status: '예약' | '완료' | '승인';
@@ -7,6 +5,14 @@ export type Reservation = {
 
 type BookedBoxProps = {
   reservations: Reservation[];
+  resData: {
+    date: string;
+    reservations: {
+      completed: number;
+      confirmed: number;
+      pending: number;
+    };
+  }[];
 };
 
 const statusStyles = {
@@ -15,14 +21,29 @@ const statusStyles = {
   승인: 'bg-orange-light text-orange',
 };
 
-function BookedBox({ reservations }: BookedBoxProps) {
+function BookedBox({ reservations, resData }: BookedBoxProps) {
+  const getStatusCounts = (date: string) => {
+    const dataForDate = resData.find((data) => data.date === date);
+    if (dataForDate) {
+      return {
+        예약: dataForDate.reservations.pending || 0,
+        완료: dataForDate.reservations.completed || 0,
+        승인: dataForDate.reservations.confirmed || 0,
+      };
+    }
+    return { 예약: 0, 완료: 0, 승인: 0 };
+  };
+
   return (
     <div className='flex flex-col-reverse w-[100%] h-[80%] gap-[0.25rem]'>
-      {reservations.map((reservation) => (
-        <div key={`${reservation.date}-${reservation.status}`} className={`rounded ${statusStyles[reservation.status]} text-center`}>
-          {reservation.status}
-        </div>
-      ))}
+      {reservations.map((reservation) => {
+        const statusCounts = getStatusCounts(reservation.date);
+        return (
+          <div key={`${reservation.date}-${reservation.status}`} className={`rounded ${statusStyles[reservation.status]} text-center`}>
+            {reservation.status} {statusCounts[reservation.status] > 0 ? `${statusCounts[reservation.status]}` : ''}
+          </div>
+        );
+      })}
     </div>
   );
 }
