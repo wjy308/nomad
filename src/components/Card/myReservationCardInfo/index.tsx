4e5182c9ace225a3649d134af9011fa0480d8525
eunjae <rladmswo1715@naver.com>
2024-07-20 16:25:33 +0900

    Feat: 후기작성 퍼블리싱

--- src/components/Card/myReservationCardInfo/index.tsx
+++ src/components/Card/myReservationCardInfo/index.tsx
@@ -1,24 +1,38 @@
 import Button from '@/components/Button';
+import Review from '@/components/Review';
 import { IReservationCardInfo } from '@/types/ReservationInfo';
 import setReservationStatueInfo from '@/utils/setReservationStatueInfo';
+import { useState } from 'react';
 
 export default function MyActibitiyCardInfo({ data }: { data: IReservationCardInfo }) {
   const { activity, status, totalPrice, date, startTime, endTime, headCount } = data;
   const statusInfo = setReservationStatueInfo(status);
   const btnCss = 'w-[12.1rem] h-[4rem] text-[1.4rem]';
+  const [reviewModalState, setModalState] = useState(false);
+
+  const handleOpenReview = () => {
+    setModalState(true);
+  };
+
+  const handleCloseReview = () => {
+    setModalState(false);
+  };
 
   return (
-    <div className='relative flex flex-col justify-center w-full'>
-      <span className={`leading-[2.6rem] font-[700] text-[1.6rem] ${statusInfo.btnColor}`}>{statusInfo.name}</span>
-      <h3 className='text-[2rem] font-[700] leading-[2.6rem] text-[#112211] mt-[0.8rem]'>{activity.title}</h3>
-      <span className='text-[1.8rem] font-[400] leading-[2.4rem] pt-[1.2rem]'>
-        {date} · {startTime} - {endTime} · {headCount}명
-      </span>
-      <div className='flex justify-between mt-[1.6rem]'>
-        <span className='text-[2.4rem] text-[#1b1b1b] leading-[2.864rem] font-[500]'>₩{totalPrice.toLocaleString('ko-KR')}</span>
-        {statusInfo.name === '예약 신청' && <Button text='예약 취소' color='white' cssName={btnCss} />}
-        {statusInfo.name === '체험 완료' && <Button text='후기 작성' color='black' cssName={btnCss} />}
+    <>
+      <div className='relative flex flex-col justify-center w-full'>
+        <span className={`leading-[2.6rem] font-[700] text-[1.6rem] ${statusInfo.btnColor}`}>{statusInfo.name}</span>
+        <h3 className='text-[2rem] font-[700] leading-[2.6rem] text-[#112211] mt-[0.8rem]'>{activity.title}</h3>
+        <span className='text-[1.8rem] font-[400] leading-[2.4rem] pt-[1.2rem]'>
+          {date} · {startTime} - {endTime} · {headCount}명
+        </span>
+        <div className='flex justify-between mt-[1.6rem]'>
+          <span className='text-[2.4rem] text-[#1b1b1b] leading-[2.864rem] font-[500]'>₩{totalPrice.toLocaleString('ko-KR')}</span>
+          {statusInfo.name === '예약 신청' && <Button text='예약 취소' color='white' cssName={btnCss} />}
+          {statusInfo.name === '체험 완료' && <Button text='후기 작성' color='black' cssName={btnCss} onClick={handleOpenReview} />}
+        </div>
       </div>
-    </div>
+      {reviewModalState && <Review closeModal={handleCloseReview} reservationInfo={data} />}
+    </>
   );
 }
---
+++ src/components/Review/ReviewForm/index.tsx
@@ -0,0 +1,47 @@
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import postMyReview from '@/apis/post/postMyReview';
+
+import Button from '@/components/Button';
+import RatingInput from '../RatingInput';
+
+interface FormData {
+  rating: number;
+  content: string;
+}
+
+interface Props {
+  id: number;
+  onClickCloseModal: () => void;
+}
+
+export default function ReviewForm({ id, onClickCloseModal }: Props) {
+  const queryClient = useQueryClient();
+  const { control, handleSubmit, setValue, register } = useForm<FormData>({
+    defaultValues: { rating: 0, content: '' },
+  });
+
+  const postReviewMutation = useMutation({
+    mutationFn: (data: FormData) => postMyReview(id, data),
+    onSuccess: () => {
+      onClickCloseModal();
+      queryClient.invalidateQueries({ queryKey: ['MyReservations'] });
+    },
+  });
+
+  const submit: SubmitHandler<FormData> = (data) => {
+    postReviewMutation.mutate(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(submit)} className='flex flex-col gap-[2.4rem]'>
+      <RatingInput control={control} setValue={setValue} />
+      <textarea
+        {...register('content', { required: '후기를 작성해 주세요' })}
+        className='text-[1.6rem] border border-gray-400 w-full min-h-[22.4rem] resize-none p-2 px-4 rounded-md'
+        placeholder='후기를 작성해주세요'
+      />
+      <Button color='black' text='작성하기' cssName='py-[1.5rem] text-[1.6rem] leading-[2.6rem] font-[700]' />
+    </form>
+  );
+}
