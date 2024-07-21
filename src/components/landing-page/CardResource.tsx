d12180e296c4d1cb7ebda92cb378a3d509f0309e
SeungRyeolBaek <bsr20030507@g.skku.edu>
2024-07-21 13:07:57 +0900

    fix: 메인 페이지 카드 텍스트 조정

--- src/components/landing-page/CardResource.tsx
+++ src/components/landing-page/CardResource.tsx
@@ -32,16 +32,20 @@ export default function CardResource({ activitiesData, banner }: CardResourcePro
   return (
     <div
       onClick={() => handleClick(activitiesData.id)}
-	  onMouseEnter={handleMouseEnter}
+      onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
       className={`flex flex-col gap-4 cursor-pointer bg-gradient-to-b 
 		to-[#000000cc] transition-transform duration-300
-		${banner ? 'relative md:w-[38.4rem] md:h-[38.4rem] w-[18.6rem] h-[18.6rem] from-transparent rounded-[2.4rem] flex-col-reverse' 
-			: ''}`}
+		${banner ? 'relative md:w-[38.4rem] md:h-[38.4rem] w-[18.6rem] h-[18.6rem] from-transparent rounded-[2.4rem] flex-col-reverse' : ''}`}
     >
-      <div className={`${banner ? 'absolute inset-0 z-[-1] md:w-[38.4rem] md:h-[38.4rem] w-[18.6rem] h-[18.6rem]' 
-	  : 'relative lg:w-[28.3rem] lg:h-[28.3rem] md:w-[22.1rem] md:h-[22.1rem] w-[16.8rem] h-[16.8rem] rounded-[1.6rem]'} 
-	   overflow-hidden md:w-full md:pt-full md:h-auto `}>
+      <div
+        className={`${
+          banner
+            ? 'absolute inset-0 z-[-1] md:w-[38.4rem] md:h-[38.4rem] w-[18.6rem] h-[18.6rem]'
+            : 'relative lg:w-[28.3rem] lg:h-[28.3rem] md:w-[22.1rem] md:h-[22.1rem] w-[16.8rem] h-[16.8rem] rounded-[1.6rem]'
+        } 
+	   overflow-hidden md:w-full md:pt-full md:h-auto `}
+      >
         <Image
           src={activitiesData.bannerImageUrl}
           width={384}
@@ -58,14 +62,25 @@ export default function CardResource({ activitiesData, banner }: CardResourcePro
           &nbsp;
           <span className='text-[#a1a1a1]'>({activitiesData.reviewCount})</span>
         </div>
-        <div className={`${banner ? 'md:w-[23rem] md:h-[7.2rem] w-[14.6rem] h-[7.2rem] md:leading-[3.6rem] ledading-[2.15rem] md:text-[3rem] text-[1.8rem]' : ''} 
-		text-2xl font-semibold line-clamp-1 md:text-lg`}>{activitiesData.title}</div>
-        <div className='flex items-center text-3xl font-bold gap-2 md:text-2xl'>
+        <div
+          className={`${banner ? 'md:w-[23rem] md:h-[7.2rem] w-[14.6rem] h-[7.2rem] md:leading-[3.6rem] ledading-[2.15rem] md:text-[3rem] text-[1.8rem]'
+			 : 'md:text-[2.4rem] md:leading-[2.86rem] text-[1.8rem] leading-[2.15rem]'} 
+		font-semibold line-clamp-1`}
+        >
+          {activitiesData.title}
+        </div>
+        <div
+          className={` ${banner ? 'md:leading-[2.4rem] md:text-[2rem] leading-[1.9rem] text-[1.6rem] md:h-[2.4rem] h-[1.9rem]' 
+			: 'md:leading-[3.34rem] md:text-[2.8rem] leading-[2.4rem] text-[2rem] md:h-[3.3rem] h-[2.4rem]'} 
+			flex items-center font-bold gap-2 `}
+        >
           {activitiesData.price === 0 ? (
             '무료체험'
           ) : (
             <>
-              ￦ {activitiesData.price.toLocaleString()} <span className='text-2xl font-normal text-[#a4a1aa] md:text-lg'>/ 인</span>
+              ￦ {activitiesData.price.toLocaleString()} 
+			  <span className={`${banner ? 'text-[1.4rem] leading-[1.67rem]' : 'md:leading-[2.4rem] md:text-[2rem] text-[1.6rem] leading-[1.9rem]'} 
+			  font-normal text-[#a4a1aa]`}>/ 인</span>
             </>
           )}
         </div>
