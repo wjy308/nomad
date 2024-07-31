bc534ada03d6c662fc65167dd9f0b0dad7be0d6d
eunjae <rladmswo1715@naver.com>
2024-07-31 15:48:38 +0900

    Fix: 반응형 브레이크포인트 수정

--- src/components/SideNavigation/index.tsx
+++ src/components/SideNavigation/index.tsx
@@ -3,7 +3,7 @@ import NavigationItem from './NavigationItem';
 
 function SideNavigation() {
   return (
-    <div className='md:w-[38.4rem] w-[25rem] border border-[#DDDDDD] shadow-[0_0.4rem_1.6rem_0_rgba(17,34,17,0.05)] p-[2.4rem] rounded-[1.2rem] flex flex-col gap-[2.4rem] items-center bg-[#FFFFFF] shrink-0 dark:bg-nomad-black'>
+    <div className='lg:w-[38.4rem] w-[25rem] border border-[#DDDDDD] shadow-[0_0.4rem_1.6rem_0_rgba(17,34,17,0.05)] p-[2.4rem] rounded-[1.2rem] flex flex-col gap-[2.4rem] items-center bg-[#FFFFFF] shrink-0 dark:bg-nomad-black'>
       <ProfileImage />
       <NavigationItem />
     </div>
